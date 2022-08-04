f486e8d4ab039f6ff56404542b392f3c015176f4
Eric Stanley <ericstanley@pop-os.localdomain>
2022-08-04 23:28:20 +0530

    first commit

--- src/config.ts
+++ src/config.ts
@@ -1,27 +1,26 @@
 export default {
-    "db": {
-        "user": null,
-        "pass": null,
-        "host": "localhost",
-        "port": "27017",
-        "database": "testdb",
-        "authSource": null
-    },
-    "host": {
-        "url": "<server-url>",
-        "port": "3000"
-    },
-    "jwt": {
-        "secretOrKey": "secret",
-        "expiresIn": 36000000
-    },
-    "mail":{
-        "host": "<smtp-host>",
-        "port": "<port>",
-        "secure": false,
-        "user": "<username>",
-        "pass": "<password>"
-    }
-  }
-  
-  
+  db: {
+    user: null,
+    pass: null,
+    host: "127.0.0.1",
+    port: "27017",
+    database: "email-auth",
+    authSource: null,
+  },
+  host: {
+    url: "http://localhost",
+    port: "3000",
+  },
+  jwt: {
+    secretOrKey: "secret",
+    expiresIn: 36000000,
+  },
+  mail: {
+    host: "localhost",
+    port: "1025",
+    secure: false,
+    user: "project.1",
+    pass: "secret.1",
+    email: "test@example.com",
+  },
+};
